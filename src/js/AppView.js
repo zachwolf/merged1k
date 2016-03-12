7742180855c4b1f05bdb7f697d46cb94b33ef4c8
Zach Wolf <hello@zachwolf.com>
2016-03-11 22:36:20 -0600

    adds random piece generation

--- src/js/AppView.js
+++ src/js/AppView.js
@@ -2,16 +2,22 @@
  * Contains all other views
  */
 var AppView = function (options) {
-	// save references to passed paramaters
-	this.canvas  = options.canvas
-	this.context = options.context
-	this.body    = options.body
+  // save references to passed paramaters
+  this.canvas  = options.canvas
+  this.context = options.context
+  this.body    = options.body
 
-	// creating child views
-	this.board = new BoardView()
+  // creating child views
+  this.board = new BoardView()
 
-	// binding events
-	this.bindEvents()
+  // game model
+  this.__model = {
+    currentPiece: null
+    // ...
+  }
+
+  // binding events
+  this.bindEvents()
 }
 
 /**
@@ -19,26 +25,26 @@ var AppView = function (options) {
  * @return {[type]} [description]
  */
 AppView.prototype.bindEvents = function () {
-	this.canvas.addEventListener('mousedown', this.handleDragEvent.bind(this))
+  this.canvas.addEventListener('mousedown', this.handleDragEvent.bind(this))
 
-	this.body.addEventListener('mouseup', this.handleReleaseEvent.bind(this))
+  this.body.addEventListener('mouseup', this.handleReleaseEvent.bind(this))
 }
 
 /**
  * 
  */
 AppView.prototype.handleDragEvent = function (e) {
-	e.preventDefault()
+  e.preventDefault()
 
-	console.log('handle drag', this);
+  console.log('handle drag', this);
 }
 
 /**
  * 
  */
 AppView.prototype.handleReleaseEvent = function (e) {
-	e.preventDefault()
-	console.log('handle drag', this);
+  e.preventDefault()
+  console.log('handle drag', this);
 }
 
 /**
@@ -50,7 +56,20 @@ AppView.prototype.handleReleaseEvent = function (e) {
  * @return {[type]} [description]
  */
 AppView.prototype.startGame = function() {
-	/*
-	clear board...
-	 */
+  /*
+  clear game model
+  set a new piece
+   */
+  
+  var highest     = this.board.getHighestValue()
+    , numberLimit = !~~highest ? 2 : highest
+
+  this.__model.currentPiece = new PieceView(function (possibleList) {
+    return possibleList.filter(function (possible) {
+      return possible.length === 1 ?
+               possible[0] <= numberLimit :
+               possible[0] <= numberLimit &&
+               possible[1] <= numberLimit
+    })
+  })
 }
--- src/js/app.js
+++ src/js/app.js
@@ -7,17 +7,42 @@ var BoardView = function () {
 	
 }
 
-/**
- * A single piece to be added to the board
- * 
- * @constructor
- */
-var PieceView = function () {
-
+BoardView.prototype.getHighestValue = function () {
+	return 0
 }
 
+/**
+ * A single piece to be added to the board
+ *
+ * @param {Function} filter - a way for the model to specify possible pieces
+ * @constructor
+ */
+var PieceView = function (filter) {
+	this.__model = {
+		value: null,
+		possibilities: filter([
+			[1],    [2],    [3],    [4],    [5],    [6],   [7],
+			        [2, 1], [3, 1], [4, 1], [5, 1], [6, 1],
+			                [3, 2], [4, 2], [5, 2], [6, 2],
+			                        [4, 3], [5, 3], [6, 3],
+			                                [5, 4], [6, 4],
+			                                        [6, 5]
+		])
+	}
 
+	this.setValue()
+}
 
+/**
+ * Sets the board pieces value
+ */
+PieceView.prototype.setValue = function (__seed) {
+	this.__model.value = __seed || (function(p){
+		return p[Math.floor(Math.random() * p.length)]
+	}(this.__model.possibilities))		
+
+	return this
+}
 
 // make it happ'n, cap'n
 var app = new AppView({
