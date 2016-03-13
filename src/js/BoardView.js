9b72de0cf2fbcf41deb6bbad8641863a408ae1a3
Zach Wolf <hello@zachwolf.com>
2016-03-13 06:36:54 -0500

    semi buggy adding piece to stage

--- src/js/BoardView.js
+++ src/js/BoardView.js
@@ -1,32 +1,32 @@
 function validateDrop (m, piece){
-	// if first block is on an occupied piece
-	if (m.xy0 !== 0) {
-		return false
-	}
+  // if first block is on an occupied piece
+  if (m.rowCol0 !== 0) {
+    return false
+  }
 
-	if (piece.length === 2) {
-		m.x1 = m.x0 + (!!~[0,180].indexOf(piece.__model.rotation) ? 1 : 0)
-		m.y1 = m.y0 + (!!~[0,180].indexOf(piece.__model.rotation) ? 0 : 1)
+  if (piece.length === 2) {
+    m.row1 = m.row0 + (!!~[0,180].indexOf(piece.__model.rotation) ? 0 : 1)
+    m.col1 = m.col0 + (!!~[0,180].indexOf(piece.__model.rotation) ? 1 : 0)
 
-		// two piece is off board to bottom
-		if (m.y1 >= this.__model.rowLimit) {
-			return false
-		}
+    // two piece is off board to bottom
+    if (m.col1 >= this.__model.rowLimit) {
+      return false
+    }
 
-		// two piece is off board to right
-		if (m.x1 >= this.__model.colLimit) {
-			return false
-		}
+    // two piece is off board to right
+    if (m.col1 >= this.__model.colLimit) {
+      return false
+    }
 
-		m.xy1 = this.__model.state[m.y1][m.x1]
+    m.rowCol1 = this.__model.state[m.row1][m.col1]
 
-		// if second block is on an occupied piece
-		if (m.xy1 !== 0) {
-			return false
-		}
-	}
+    // if second block is on an occupied piece
+    if (m.rowCol1 !== 0) {
+      return false
+    }
+  }
 
-	return true
+  return true
 }
 
 /**
@@ -35,14 +35,14 @@ function validateDrop (m, piece){
  * @constructor
  */
 var BoardView = function () {
-	this.__model = {
-		state: []
-	}
+  this.__model = {
+    state: []
+  }
 
-	this.reset()
+  this.reset()
 
-	this.__model.rowLimit = this.__model.state.length
-	this.__model.colLimit = this.__model.state[0].length
+  this.__model.rowLimit = this.__model.state.length
+  this.__model.colLimit = this.__model.state[0].length
 }
 
 /**
@@ -50,7 +50,7 @@ var BoardView = function () {
  * @return {[type]} [description]
  */
 BoardView.prototype.getHighestValue = function () {
-	return 0
+  return 0
 }
 
 /**
@@ -58,9 +58,9 @@ BoardView.prototype.getHighestValue = function () {
  * @return {[type]} [description]
  */
 BoardView.prototype.reset = function () {
-	this.__model.state = CONFIG.BOARD.DEFAULT
+  this.__model.state = CONFIG.BOARD.DEFAULT
 
-	return this
+  return this
 }
 
 /**
@@ -69,101 +69,318 @@ BoardView.prototype.reset = function () {
  * @return {[type]}     [description]
  */
 BoardView.prototype.getRowColumn = function(pos) {
-	var x  = pos.x || NaN
-		, y  = pos.y || NaN
-		, cx = x + CONFIG.PIECE.SIZE - CONFIG.BOARD.X
-		, cy = y + CONFIG.PIECE.SIZE - CONFIG.BOARD.Y
-		, isOverBoard = CONFIG.BOARD.X < cx && cx < CONFIG.BOARD.X + CONFIG.BOARD.WIDTH &&
-										CONFIG.BOARD.Y < cy && cy < CONFIG.BOARD.Y + CONFIG.BOARD.HEIGHT
+  var x  = pos.x || NaN
+    , y  = pos.y || NaN
+    , cx = x + CONFIG.PIECE.SIZE - CONFIG.BOARD.X
+    , cy = y + CONFIG.PIECE.SIZE - CONFIG.BOARD.Y
+    , isOverBoard = CONFIG.BOARD.X < cx && cx < CONFIG.BOARD.X + CONFIG.BOARD.WIDTH &&
+                    CONFIG.BOARD.Y < cy && cy < CONFIG.BOARD.Y + CONFIG.BOARD.HEIGHT
 
-	if (isOverBoard) {
-		var dropX = Math.floor(((cx - CONFIG.BOARD.X) / CONFIG.BOARD.WIDTH) * 5)
-			, dropY = Math.floor(((cy - CONFIG.BOARD.Y) / CONFIG.BOARD.HEIGHT) * 5)
+  if (isOverBoard) {
+    var dropX = Math.floor(((cx - CONFIG.BOARD.X) / CONFIG.BOARD.WIDTH) * 5)
+      , dropY = Math.floor(((cy - CONFIG.BOARD.Y) / CONFIG.BOARD.HEIGHT) * 5)
 
-		return [dropX, dropY]
-	} else {
-		return null
-	}
+    return [dropY, dropX]
+  } else {
+    return null
+  }
 }
 
 /**
  * 
  */
-BoardView.prototype.trySet = function (xy, piece) {
-	var posMap = {
-				x0: xy[0],
-				y0: xy[1],
-				xy0: this.__model.state[xy[1]][xy[0]],
-				x1: NaN,
-				y1: NaN,
-				xy1: NaN
-			}
-		// can be set logic
-		, validDrop = validateDrop.call(this, posMap, piece)
+// BoardView.prototype.trySet = function (xy, piece) {
+BoardView.prototype.trySet = function (rowCol, piece) {
+  var posMap = {
+        row0: rowCol[0],
+        col0: rowCol[1],
+        rowCol0: this.__model.state[rowCol[0]][rowCol[1]],
+        row1: NaN,
+        col1: NaN,
+        rowCol1: NaN
+      }
+    // can be set logic
+    , validDrop = validateDrop.call(this, posMap, piece)
 
-	if (validDrop) {
-		// todo set correct positioning of complex pieces
-		this.__model.state[posMap.y0][posMap.x0] = piece.__model.value[0]
+  if (validDrop) {
+    this.__model.state[posMap.row0][posMap.col0] = piece.__model.value[0]
 
-		if (piece.length === 2) {
-			this.__model.state[posMap.y1][posMap.x1] = piece.__model.value[1]
-		}
-	}
-
-	return validDrop
-}
-
-/**
- * 
- */
-function crawlFromPosition (val, origin) {
-	var row = origin[0]
-		, nextRow = row + 1
-		, col = origin[1]
-		, nextCol = col + 1
-		, prevCol = col - 1
-		, positions = [[row, col]]
-
-	if (nextCol < this.__model.colLimit) {
-		if (this.__model.state[row][nextCol] === val) {
-			positions = positions.concat(crawlFromPosition.call(this, val, [row, nextCol]))
-		}
-	}
-
-	if (nextRow < this.__model.rowLimit) {
-		if (this.__model.state[nextRow][col] === val) {
-			positions = positions.concat(crawlFromPosition.call(this, val, [nextRow, col]))
-		}
-	}
-
-	return positions
-}
-
-/**
- * 
- */
-BoardView.prototype.checkForMerges = function (xyOrigin) {
-	var state = this.__model.state
-
-  for (var rowkey = 0; rowkey < this.__model.rowLimit; rowkey++) {
-  	var row = state[rowkey]
-
-	  for (var colkey = 0; colkey < this.__model.colLimit; colkey++) {
-	  	var sqr = row[colkey]
-
-	  	if (sqr !== 0) {
-	  		var toBeMerged = crawlFromPosition.call(this, sqr, [rowkey, colkey]) // todo: is col, row correct or reversed?
-
-	  		if (toBeMerged.length >= 3) {
-	  			console.log('merge', toBeMerged);
-	  			// toBeMerged.forEach(function (argument) {
-	  			// 	// body...
-	  			// })
-	  			break
-	  		}
-	  	}
-	  }
+    if (piece.length === 2) {
+      this.__model.state[posMap.row1][posMap.col1] = piece.__model.value[1]
+    }
   }
 
-	return this
+  return validDrop
 }
+
+/**
+ * 
+ */
+function crawlPositions (sqrs) {
+  if (sqrs.length < 3) {
+    return false
+  }
+
+  var res    = [sqrs[0]]
+    , sqrStr = sqrs.map(function (sqr) {
+        return sqr.toString()
+      })
+    , pairs  = []
+
+  // var above = not needed?
+  sqrs.forEach((function (sqr) {
+    var col   = sqr[0]
+      , row   = sqr[1]
+      , left  = row + 1
+      , right = row + 1
+      , above = col - 1
+      , below = col + 1
+
+    if (above >= 0) {
+      if (!!~sqrStr.indexOf([above, row].toString())) {
+        pairs.push([sqr, [above, row]])
+      }
+    }
+
+    if (below < this.__model.colLimit) {
+      if (!!~sqrStr.indexOf([below, row].toString())) {
+        pairs.push([sqr, [below, row]])
+      }
+    }
+
+    if (right < this.__model.rowLimit) {
+      if (!!~sqrStr.indexOf([col, right].toString())) {
+        pairs.push([sqr, [col, right]])
+      }
+    }
+
+    if (left >= 0) {
+      if (!!~sqrStr.indexOf([col, left].toString())) {
+        pairs.push([sqr, [col, left]])
+      }
+    }
+  }).bind(this))
+
+  var pairStr = pairs.map(function (pair) {
+    return pair.map(function (rowCol) {
+      return rowCol.toString()
+    })
+  })
+
+  var group = filterGroups.call(this, pairs)
+
+  console.log('group', group);
+  if (group && group.length && group[0].length < 3) {
+    return false
+  }
+
+  return group[0].map(function (item) {
+    var split = item.split(',')
+    return [parseInt(split[0]), parseInt(split[1])]
+  })
+}
+
+/**
+ * [function_name description]
+ * @param  {[type]} argument [description]
+ * @return {[type]}          [description]
+ */
+function filterGroups (groups, _res) {
+  var res = _res || []
+    , possibleRes = []
+    , pairStr = groups.map(function (group) {
+        return group.filter(function(item, pos, self) {
+          return self.indexOf(item) == pos
+        }).sort().toString()
+      }).filter(function(item, pos, self) {
+        // if (self.indexOf(item) == pos && item.match(/\d,\d/g).length >= 3) {
+        //   possibleRes.push(item.match(/\d,\d/g))
+        // }
+        return self.indexOf(item) == pos
+      }).map(function (item) {
+        return item.match(/\d,\d/g)
+      })
+    , newGroups = []
+
+  if (pairStr.length > 1) {
+    pairStr.forEach(function (groupa, keya) {
+      pairStr.forEach(function (groupb, keyb) {
+        if (keya !== keyb) {
+          if (!!~groupb.indexOf(groupa[0]) || !!~groupb.indexOf(groupa[1])) {
+            newGroups.push(groupa.concat(groupb))
+          }
+        }
+      })
+    })
+
+    if (limit >= 0) {
+      limit = limit - 1
+
+      var singleGroup = filterGroups.call(this, newGroups)
+
+      // var otherGroups = groups.filter(function (pos) {
+      //   for (var singleGroupKey = 0; singleGroupKey < singleGroup.length; singleGroupKey++) {
+      //     if (!!~pos.indexOf(singleGroup[0][singleGroupKey])) {
+      //       return false
+      //     }
+      //   }
+
+      //   return true
+      // })
+
+      // if (otherGroups.length) {};
+      return singleGroup
+    }
+  } else {
+    return pairStr
+  }
+
+  // return res
+  return newGroups
+}
+
+var limit = 10
+
+/**
+ * 
+ */
+BoardView.prototype.checkForMerges = function (rowColOrigin) {
+  var state = this.__model.state
+    , activeNumber = {}
+    , self = this
+
+  state.forEach(function (row, rowkey) {
+    row.forEach(function (sqr, colkey) {
+      if (sqr !== 0) {
+        if (!activeNumber[sqr]) {
+          activeNumber[sqr] = []
+        }
+
+        activeNumber[sqr].push([rowkey, colkey])
+      }
+    })
+  })
+
+  for (var valkey in activeNumber) {
+    var toBeMerged = crawlPositions.call(this, activeNumber[valkey])
+
+    if (toBeMerged) {
+      toBeMerged.forEach(function (sqr) {
+        if (sqr.toString() === rowColOrigin.toString()) {
+          state[rowColOrigin[0]][rowColOrigin[1]] = state[rowColOrigin[0]][rowColOrigin[1]] + 1
+        } else {
+          state[sqr[0]][sqr[1]] = 0
+        }
+      })
+    }
+  }
+
+  return this
+}
+
+
+
+/*
+
+
+
+var row = origin[0]
+    , nextRow = row + 1
+    , col = origin[1]
+    , nextCol = col + 1
+    , prevCol = col - 1
+    , positions = [[row, col]]
+
+  if (nextCol < this.__model.colLimit) {
+    if (this.__model.state[row][nextCol] === val) {
+      positions = positions.concat(crawlFromPosition.call(this, val, [row, nextCol]))
+    }
+  }
+
+  if (nextRow < this.__model.rowLimit) {
+    if (this.__model.state[nextRow][col] === val) {
+      positions = positions.concat(crawlFromPosition.call(this, val, [nextRow, col]))
+    }
+  }
+
+  return positions
+
+
+
+
+for (var sqrkey = 0; sqrkey < sqrs.length; sqrkey++) {
+    var sqr = sqrs[sqrkey]
+      , row     = sqr[0]
+      , nextRow = row + 1
+      , col     = sqr[1]
+      , nextCol = col + 1
+
+    if (nextCol < this.__model.colLimit && !!~strs.indexOf([row, nextCol].toString())) {
+      res.push([row, nextCol])
+    }
+
+    if (nextRow < this.__model.rowLimit && !!~strs.indexOf([nextRow, col].toString())) {
+      res.push([nextRow, col])
+    }
+  }
+
+
+
+
+for (var rowkey = 0; rowkey < this.__model.rowLimit; rowkey++) {
+    var row = state[rowkey]
+
+    for (var colkey = 0; colkey < this.__model.colLimit; colkey++) {
+      var sqr = row[colkey]
+
+      if (sqr !== 0) {
+        var toBeMerged = crawlFromPosition.call(this, sqr, [rowkey, colkey]) // todo: is col, row correct or reversed?
+
+        if (toBeMerged.length >= 3) {
+          console.log('merge', toBeMerged);
+          // toBeMerged.forEach(function (argument) {
+          //  // body...
+          // })
+          break
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+debugger
+
+      var nextPass = filterGroups.call(this, newGroups)
+
+      debugger
+
+      var allPairs = pairStr.reduce(function (curr, next) {
+        return curr.concat(next)
+      })
+
+
+
+
+
+      var x = possibleRes.filter(function (pos) {
+        for (var pairkey = 0; pairkey < allPairs.length; pairkey++) {
+          if (!!~pos.indexOf(allPairs[pairkey])) {
+            return false
+          }
+        }
+
+        return true
+      })
+
+
+
+
+ */
